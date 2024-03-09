2f3a2a3b29a7280b65bd8c3348cd2f53aa3b07f0
Aditya Kumar Sinha <adityakumar113141@gmail.com>
2024-03-09 17:13:12 +0530

    Update mongodb.js

--- Database/mongodb.js
+++ Database/mongodb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const password = process.env.MONGODB_PASSWORD;
 const userName=process.env.MONGODB_USER_NAME;
 function connection() {
-    mongoose.connect(`mongodb+srv://${userName}:${password}@cluster0.sjwfxza.mongodb.net/intern?retryWrites=true&w=majority`).then(() => {
+    mongoose.connect(`mongodb+srv://${userName}:${password}@cluster0.izbkr1o.mongodb.net/testdata?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
         console.log("mongodb database Connected  Successfully");
     }).catch((err) => {
         console.log("Error in mongodb databse Connection " + err);
